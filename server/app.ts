07393282e0b267c8d8d2ea010927d99b67e9aed4
SyeddHassan <syeddhassanali@gmail.com>
2025-02-17 06:02:24 +0500

    made some changes regarding cors in backend

--- server/app.ts
+++ server/app.ts
@@ -17,6 +17,8 @@ app.use(
   cors({
     origin: `${process.env.CLIENT_SIDE_URL}`,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
